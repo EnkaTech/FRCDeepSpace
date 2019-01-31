8e42737a8eeb598f832ccbe4670ec927f8910e0c
berkan <tt.berkan092@gmail.com>
2019-01-31 20:10:44 +0300

    Rangefinder ile yaklasma eklendi

--- src/main/java/frc/robot/commands/Align.java
+++ src/main/java/frc/robot/commands/Align.java
@@ -14,24 +14,8 @@ public class Align extends CommandGroup {
    * Add your docs here.
    */
   public Align() {
-    // Add Commands here:
-    // e.g. addSequential(new Command1());
-    // addSequential(new Command2());
-    // these will run in order.
-    //addSequential(new GyroTurn(-90));
     addSequential(new AlignRotate());
-    //addSequential(new GyroDriveY(2, !RobotMap.error));
     addSequential(new AlignH());
-    // To run multiple commands at the same time,
-    // use addParallel()
-    // e.g. addParallel(new Command1());
-    // addSequential(new Command2());
-    // Command1 and Command2 will run in parallel.
-
-    // A command group will require all of the subsystems that each member
-    // would require.
-    // e.g. if Command1 requires chassis, and Command2 requires arm,
-    // a CommandGroup containing them would require both the chassis and the
-    // arm.
+    addSequential(new Approach(35));
   }
 }
---
+++ src/main/java/frc/robot/commands/Approach.java
@@ -0,0 +1,58 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+import frc.robot.RobotMap;
+
+public class Approach extends Command {
+  double currentDist;
+  double wantedDist;
+  double startingAngle;
+  double error;
+  double power;
+  final double kP = 0.02;
+  public Approach(double wantedDistance) {
+    // Use requires() here to declare subsystem dependencies
+    requires(Robot.driveTrain);
+    wantedDist = wantedDistance;
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+    startingAngle = RobotMap.gyro.getAngle();
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    currentDist = RobotMap.getDistance();
+    error = currentDist-wantedDist;
+    power = error*kP;
+    Robot.driveTrain.gyroDriveX(RobotMap.gyro, power, startingAngle);
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return Math.abs(error) <= 15;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
